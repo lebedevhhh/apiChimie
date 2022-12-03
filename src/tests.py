cb8b499fdf7d22bfcc8177a876f03e83579f7389
lebedevhh <lebedevhh@outlook.fr>
2022-12-02 22:54:22 -0500

    made some put configs

--- src/tests.py
+++ src/tests.py
@@ -11,16 +11,16 @@ import json
 #             charges = args["charges"]
 # )
 
-url = "http://127.0.0.1:5000/atome/11" #<int:numprotons>
+url = "http://127.0.0.1:5000/atome/28" #<int:numprotons>
 
 dt = {
-    'name' :"Sodium",
-    'val_e': 1,
-    'e_negativity': 0.93,
-    'Masse_Molaire': 22.99,
-    'config_e_quick': "[Ne] 3(s1)",
-    'config_e_full': "",
-    'charges': "(0)"
+    'name' :"Cobalt",
+    'val_e': 9,
+    'e_negativity': 1.88,
+    'Masse_Molaire': 55.85,
+    'config_e_quick': "[Ar] 4(s2) 3(d7)",
+    'config_e_full': "1(s2) 2(s2) 2(p6) 3(s2) 3(p6) 4(s2) 3(d7)",
+    'charges': "(+2, +3)"
 }
 
 
@@ -29,4 +29,5 @@ print(resp.status_code)
 
 ##
 ##need to change the carone full config and also quick config
-##
+##revoir vanadium config
+#### rendu Nickel
