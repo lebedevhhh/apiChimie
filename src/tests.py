32314e37f460d352a18064eaa711fe8688ed2823
lebedevhh <lebedevhh@outlook.fr>
2022-12-11 21:03:38 -0500

    Made some pushes into the db

--- src/tests.py
+++ src/tests.py
@@ -11,25 +11,26 @@ import json
 #             charges = args["charges"]
 # )
 
-url = "http://127.0.0.1:5000/atome/55" ##<int:numprotons>
+url = "http://127.0.0.1:5000/atome/71" ##<int:numprotons>
 
 dt = {
-    'name' :"Cesium",
-    'val_e': 1,
-    'e_negativity': 0.79,
-    'Masse_Molaire': 132.9,
-    'config_e_quick': "[Xe] 6(s1)",
-    'config_e_full': "1(s2) 2(s2) 2(p6) 3(s2) 3(p6) 4(s2) 3(d10) 4(s2) 4(p6) 5(s2) 4(d10) 5(p6) 6(s1)",
-    'charges': "(+1)"
+    'name' : "Lutétium",
+    'val_e': 3,
+    'e_negativity': 1.27,
+    'Masse_Molaire': 175.0,
+    'config_e_quick': "[Xe] 6(s2) 4(f14) 5(d1)",
+    'config_e_full': "1(s2) 2(s2) 2(p6) 3(s2) 3(p6) 4(s2) 3(d10) 4(s2) 4(p6) 5(s2) 4(d10) 5(p6) 6(s2) 4(f14) 5(d1)",
+    'charges': "(+3)"
 }
 
 
 resp = requests.put(url, data = dt)
 print(resp.status_code)
+print(resp.content)
 
 ##
+##delete 57,58,59 update
 ##need to change the carone full config and also quick config
 ##revoir vanadium config
-#### rendu Palladium
 #### revoir le bay du zinc
 ##revoir le Palladium
